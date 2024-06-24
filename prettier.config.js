f572bdc072466d41071fa64c81fae00369e0dcf4
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-06-25 07:40:18 +0800

    add prettier-plugin-sort-imports to sort imports using prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,20 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
-  plugins: ["prettier-plugin-tailwindcss"],
+  arrowParens: 'avoid',
+  printWidth: 120,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'consistent',
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+  importOrder: [
+    '~/env',
+    '^react',
+    '^next',
+    '<THIRD_PARTY_MODULES>',
+    '^[./](?!.*\\.(css|scss|less|styl|png|jpg|jpeg|gif|svg|ttf|woff|woff2|eot|otf)$)',
+    '^[./]',
+  ],
 };
 
 export default config;
