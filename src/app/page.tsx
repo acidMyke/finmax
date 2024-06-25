fdd6926a78b92503d103320f15b3882e826a81ac
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-06-25 12:32:10 +0800

    changing assets format
    make favicon transparent
    and changing asset from svg to react component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Link from 'next/link';
+import AppName from './_components/AppName';
+
 export default async function Home() {
   return (
-    <main>
-      <div>Hello World</div>
+    <main className='container mx-auto flex min-h-screen flex-col items-center justify-start'>
+      {/* Header with AppName */}
+      <header className='navbar'>
+        <Link href='/'>
+          <AppName />
+        </Link>
+      </header>
+      {/* Main content */}
+      {/* Advertise the app */}
+      <section className='mt-6 flex flex-col items-center justify-center'>
+        <h1 className='text-center text-4xl font-bold'>The best way to manage your finances</h1>
+        <p className='text-center'>Personal finance management made easy.</p>
+      </section>
     </main>
   );
 }
