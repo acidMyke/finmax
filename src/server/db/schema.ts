98fa03eb03e5ecf90eb217801fb292eb61390106
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-06-25 09:30:24 +0800

    create 6 table schema, refactor reference declaration accounts, methods, categories, payees, sub

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,5 @@
 // Example model schema from the Drizzle docs
 // https://orm.drizzle.team/docs/sql-schema-declaration
-import { sql } from 'drizzle-orm';
 import {
   boolean,
   char,
@@ -13,6 +12,8 @@ import {
   foreignKey,
   integer,
   numeric,
+  varchar,
+  AnyPgColumn,
 } from 'drizzle-orm/pg-core';
 
 /**
@@ -31,15 +32,12 @@ export const usersTable = createTable(
     pfp: text('pfp'),
     inactive: boolean('inactive').default(false),
     settings: jsonb('settings').default({}),
-    defaultAccountId: char('default_account_id', { length: 12 }),
-    defaultMethodId: char('default_method_id', { length: 12 }),
+    defaultAccountId: char('default_account_id', { length: 12 }).references((): AnyPgColumn => accountsTable.id),
+    defaultMethodId: char('default_method_id', { length: 12 }).references((): AnyPgColumn => methodsTable.id),
   },
   table => {
     return {
       clerkIdIndex: index('clerk_id_index').on(table.clerkId),
-      // TODOS
-      // defaultAccountReference: foreignKey({}),
-      // defaultMethodReference: foreignKey({}),
     };
   },
 );
@@ -51,7 +49,9 @@ export const changesTable = createTable(
   {
     id: char('id', { length: 12 }).primaryKey(),
     at: timestamp('at').notNull().defaultNow(),
-    by: char('by', { length: 12 }).notNull(),
+    by: char('by', { length: 12 })
+      .notNull()
+      .references(() => usersTable.id),
     entity: text('entity').notNull(),
     entityId: char('entity_id', { length: 12 }).notNull(),
     version: integer('version').notNull(),
@@ -63,56 +63,130 @@ export const changesTable = createTable(
     notes: text('notes'),
     metadata: jsonb('metadata'),
   },
-  table => {
-    return {
-      userReference: foreignKey({
-        columns: [table.by],
-        foreignColumns: [usersTable.id],
-        name: 'fk_changes_user',
-      }),
-      revertReference: foreignKey({
-        columns: [table.revertOf],
-        foreignColumns: [table.id],
-        name: 'fk_changes_revert',
-      }),
-    };
-  },
+  table => ({
+    revertReference: foreignKey({
+      columns: [table.revertOf],
+      foreignColumns: [table.id],
+      name: 'fk_changes_revert',
+    }),
+  }),
 );
 
 export const transactionsTable = createTable(
   'transactions',
   {
     id: char('id', { length: 12 }).primaryKey(),
-    userId: char('user_id', { length: 12 }).notNull(),
+    userId: char('user_id', { length: 12 })
+      .notNull()
+      .references(() => usersTable.id),
     date: timestamp('date').notNull(),
     amount: numeric('amount', { precision: 8, scale: 3 }).notNull(),
-    payeeId: char('payee_id', { length: 12 }).notNull(),
+    payeeId: char('payee_id', { length: 12 })
+      .notNull()
+      .references(() => payeesTable.id),
     description: text('description'),
-    accountId: char('account_id', { length: 12 }),
-    methodId: char('method_id', { length: 12 }),
-    categoryId: char('category_id', { length: 12 }),
-    subscriptionId: char('subscription_id', { length: 12 }),
+    accountId: char('account_id', { length: 12 })
+      .notNull()
+      .references(() => accountsTable.id),
+    methodId: char('method_id', { length: 12 })
+      .notNull()
+      .references(() => methodsTable.id),
+    categoryId: char('category_id', { length: 12 })
+      .notNull()
+      .references(() => categoriesTable.id),
+    subscriptionId: char('subscription_id', { length: 12 }).references(() => subscriptionsTable.id),
     parentId: char('parent_id', { length: 12 }),
     metadata: jsonb('metadata'),
   },
-  table => {
-    return {
-      userReference: foreignKey({
-        columns: [table.userId],
-        foreignColumns: [usersTable.id],
-        name: 'fk_transactions_user',
-      }),
-      parentReference: foreignKey({
-        columns: [table.parentId],
-        foreignColumns: [table.id],
-        name: 'fk_transactions_parent',
-      }),
-      // TODOS
-      // payeeReference: foreignKey({}),
-      // accountReference: foreignKey({}),
-      // methodReference: foreignKey({}),
-      // categoryReference: foreignKey({}),
-      // subscriptionReference: foreignKey({}),
-    };
-  },
+  table => ({
+    parentReference: foreignKey({
+      columns: [table.parentId],
+      foreignColumns: [table.id],
+      name: 'fk_transactions_parent',
+    }),
+  }),
 );
+
+export const accountsTable = createTable('accounts', {
+  id: char('id', { length: 12 }).primaryKey(),
+  userId: char('user_id', { length: 12 })
+    .notNull()
+    .references(() => usersTable.id),
+  label: varchar('label', { length: 64 }).notNull(),
+  currency: varchar('currency', { length: 3 }).notNull().default('SGD'),
+  iconId: char('icon_id', { length: 12 }).references(() => iconsTable.id),
+  defaultMethodId: char('default_method_id', { length: 12 }).references((): AnyPgColumn => methodsTable.id),
+  metadata: jsonb('metadata'),
+});
+
+export const methodsTable = createTable('methods', {
+  id: char('id', { length: 12 }).primaryKey(),
+  userId: char('user_id', { length: 12 })
+    .notNull()
+    .references(() => usersTable.id),
+  label: varchar('label', { length: 64 }).notNull(),
+  autoRegex: text('auto_regex'),
+  accountId: char('account_id', { length: 12 })
+    .notNull()
+    .references(() => accountsTable.id),
+  iconId: char('icon_id', { length: 12 }).references(() => iconsTable.id),
+  isPublic: boolean('is_public').default(false),
+  metadata: jsonb('metadata'),
+});
+
+export const categoriesTable = createTable('categories', {
+  id: char('id', { length: 12 }).primaryKey(),
+  userId: char('user_id', { length: 12 })
+    .notNull()
+    .references(() => usersTable.id),
+  label: varchar('label', { length: 64 }).notNull(),
+  iconId: char('icon_id', { length: 12 }).references(() => iconsTable.id),
+  isPublic: boolean('is_public').default(false),
+  metadata: jsonb('metadata'),
+});
+
+export const payeesTable = createTable('payees', {
+  id: char('id', { length: 12 }).primaryKey(),
+  userId: char('user_id', { length: 12 })
+    .notNull()
+    .references(() => usersTable.id),
+  name: varchar('name', { length: 64 }).notNull(),
+  notes: text('notes'),
+});
+
+export const paymentFrequencyEnum = pgEnum('finmax_payment_frequency', [
+  'weekly',
+  'monthly',
+  'quarterly',
+  'biannually',
+  'annually',
+]);
+
+export const subscriptionsTable = createTable('subscriptions', {
+  id: char('id', { length: 12 }).primaryKey(),
+  userId: char('user_id', { length: 12 })
+    .notNull()
+    .references(() => usersTable.id),
+  amount: numeric('amount', { precision: 8, scale: 3 }).notNull(),
+  start: timestamp('start').notNull(),
+  end: timestamp('end'),
+  frequency: paymentFrequencyEnum('frequency').notNull(),
+  payeeId: char('payee_id', { length: 12 })
+    .notNull()
+    .references(() => payeesTable.id),
+  methodId: char('method_id', { length: 12 })
+    .notNull()
+    .references(() => methodsTable.id),
+  categoryId: char('category_id', { length: 12 })
+    .notNull()
+    .references(() => categoriesTable.id),
+  accountId: char('account_id', { length: 12 })
+    .notNull()
+    .references(() => accountsTable.id),
+  metadata: jsonb('metadata'),
+});
+
+export const iconsTable = createTable('icons', {
+  id: char('id', { length: 12 }).primaryKey(),
+  data: text('data').notNull(),
+});
