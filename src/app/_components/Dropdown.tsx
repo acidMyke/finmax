5419143ccd2485ea5cbe53b3a79d958c18b05987
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-06-28 20:12:17 +0800

    created dropdown component
    installed tailwind-merge to merge tailwind classNames

---
+++ src/app/_components/Dropdown.tsx
@@ -0,0 +1,73 @@
+import { FocusEventHandler, useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { FaChevronDown } from 'react-icons/fa';
+import { twMerge } from 'tailwind-merge';
+
+type IdLabel = { id: string; label: string };
+
+interface DropdownProps {
+  options: string[] | IdLabel[];
+  option?: string;
+  onChange: (option: string) => void;
+  classNames?: {
+    detailsElement?: string;
+    summaryElement?: string;
+    ulElement?: string;
+    liElement?: string;
+  };
+  addTrailingArrow?: boolean;
+}
+
+export function Dropdown({ options, option: option, onChange, classNames, addTrailingArrow = true }: DropdownProps) {
+  const [selectedOption, setSelectedOption] = useState(
+    option ?? (typeof options[0] === 'string' ? options[0]! : options[0]!.id),
+  );
+  const detailsRef = useRef<HTMLDetailsElement>(null);
+
+  if (options.length === 0) {
+    throw new Error('Dropdown must have at least one option');
+  }
+
+  useEffect(() => {
+    setSelectedOption(option ?? (typeof options[0] === 'string' ? options[0]! : options[0]!.id));
+  }, [option, options]);
+
+  let { detailsElement, summaryElement, ulElement, liElement } = classNames || {};
+
+  const onBlur: FocusEventHandler<HTMLDetailsElement> = useCallback(e => {
+    if (!detailsRef.current?.contains(e.relatedTarget as Node)) {
+      detailsRef.current?.open && detailsRef.current?.removeAttribute('open');
+    }
+  }, []);
+
+  const selectedLabel = useMemo(() => {
+    return typeof options[0] === 'string'
+      ? selectedOption
+      : (options as IdLabel[]).find(option => option.id === selectedOption)?.label;
+  }, [options, selectedOption]);
+
+  return (
+    <details className={twMerge('group/dd dropdown', detailsElement)} onBlur={onBlur} ref={detailsRef}>
+      <summary className={twMerge('btn btn-neutral', summaryElement)}>
+        {selectedLabel}
+        {addTrailingArrow && <FaChevronDown size={16} className='transition-transform group-open/dd:rotate-180' />}
+      </summary>
+      {options.length > 0 && (
+        <ul className={twMerge('menu dropdown-content', ulElement)}>
+          {options.map(option => (
+            <li
+              key={typeof option === 'string' ? option : option.id}
+              className={twMerge(liElement)}
+              onClick={() => {
+                setSelectedOption(typeof option === 'string' ? option : option.id);
+                onChange(typeof option === 'string' ? option : option.id);
+                detailsRef.current?.removeAttribute('open');
+              }}
+            >
+              <button className='btn btn-ghost btn-sm'>{typeof option === 'string' ? option : option.label}</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </details>
+  );
+}
