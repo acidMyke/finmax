3aa9e92b419f88dc4e0070437b97244d416cc3c6
acidMyke <13977709+acidMyke@users.noreply.github.com>
2024-06-28 22:26:40 +0800

    redo middleware to make it much debuggable

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,18 @@
+import { NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { env } from './env';
 
 const isPublicRoute = createRouteMatcher(['/api/trpc(.*)', '/api/clerk', '/', '/verify', '/signed-out']);
 
-export default clerkMiddleware((auth, req) => {
-  const originUrl = new URL(req.url).origin;
+export default clerkMiddleware((getAuth, req) => {
+  const auth = getAuth();
 
   if (!isPublicRoute(req)) {
     console.log('Accessing protected route');
-    auth().protect({
-      unauthenticatedUrl: new URL('/', originUrl).href,
-      unauthorizedUrl: new URL('/', originUrl).href,
-    });
+    if (auth.userId === null) {
+      console.log('Unauthenticated user, redirecting to /');
+      return NextResponse.redirect(new URL('/', req.url));
+    }
   }
 });
 
